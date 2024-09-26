85aab67aa31f4049c29be1d21fbbef4afd50504f
iftekar2 <iftekar.hossain123@gmail.com>
2024-09-26 16:02:54 -0400

    calculating number of hours spent watching TV

--- src/main/java/com/pluralsight/Demo1.java
+++ src/main/java/com/pluralsight/Demo1.java
@@ -2,6 +2,26 @@ package com.pluralsight;
 
 public class Demo1 {
     public static void main(String[] args){
-        System.out.println("Hello World");
+        //User name
+        String name = "Iftekar";
+        //User greetings
+        String greetings = "Hello " + name;
+
+        
+        int numberOfWeeksWatchTV = 2;
+        int minutesPerDayWatchedOnAverage = 10;
+
+        int minutePerWeekWatchTV = numberOfWeeksWatchTV * minutesPerDayWatchedOnAverage;
+        int minutePerYearWatchTV = minutePerWeekWatchTV * 52;
+
+        int hourPerYear = minutePerYearWatchTV / 60;
+        int hourInWorkWeek = 35;
+
+        int numberOfHoursSpendWatchingTVInWorkWeek = hourPerYear / hourInWorkWeek;
+
+
+        System.out.println("If " + name + " did not watch TV, it would be like a " + numberOfHoursSpendWatchingTVInWorkWeek + " week vication every year.");
+
+        System.out.println(greetings);
     }
 }
